9074816b84698959f9b80df7da76f8352c71d7f6
Hoegge <hoegge@gmail.com>
2015-11-15 11:26:27 +0100

    Hold up or down implemented
    Hold up sets volume to 80, hold down to 20

--- RemoteController.m
+++ RemoteController.m
@@ -133,16 +133,11 @@
             case kHIDRemoteButtonCodeUpHold:
                 script = [[NSAppleScript alloc] initWithSource:
                           @"tell application \"Spotify\"\n\
-                          if sound volume > 5 then\n\
-                          set the sound volume to 100\n\
-                          else\n\
-                          set the sound volume to sound volume + 5\n\
-                          end if\n\
+                          set the sound volume to 80\n\
                           end tell\n\
                           "];
                 [script performSelectorOnMainThread:@selector(executeAndReturnError:) withObject:nil waitUntilDone:YES];
                 [script release];
-                
                 break;
                 
             case kHIDRemoteButtonCodeDown:
@@ -157,22 +152,16 @@
                           "];
                 [script performSelectorOnMainThread:@selector(executeAndReturnError:) withObject:nil waitUntilDone:YES];
                 [script release];
-                
                 break;
                 
             case kHIDRemoteButtonCodeDownHold:
                 script = [[NSAppleScript alloc] initWithSource:
                           @"tell application \"Spotify\"\n\
-                          if sound volume < 5 then\n\
-                          set the sound volume to 0\n\
-                          else\n\
-                          set the sound volume to sound volume - 5 \n\
-                          end if\n\
+                          set the sound volume to 20\n\
                           end tell\n\
                           "];
                 [script performSelectorOnMainThread:@selector(executeAndReturnError:) withObject:nil waitUntilDone:YES];
                 [script release];
-                
                 break;
 				
 			case kHIDRemoteButtonCodeLeft:
